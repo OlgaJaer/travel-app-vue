1c289afe58e9b761e209e1f4622be51d23e5efb0
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-08-08 11:32:46 +0200

    reacting to param changes

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section v-if="destination">
     <h1>{{ destination.name }}</h1>
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name" />
@@ -9,17 +9,34 @@
 </template>
 
 <script>
-import sourceData from "@/data.json";
+// import sourceData from "@/data.json";
 export default {
+  data() {
+    return {
+      destination: null,
+    };
+  },
   computed: {
     destinationId() {
       return parseInt(this.$route.params.id);
     },
-    destination() {
-      return sourceData.destinations.find(
-        (destination) => destination.id === this.destinationId
+    // destination() {
+    //   return sourceData.destinations.find(
+    //     (destination) => destination.id === this.destinationId
+    //   );
+    // },
+  },
+  methods: {
+    async initData() {
+      const response = await fetch(
+        `https://travel-dummy-api.netlify.app/${this.$route.params.slug}`
       );
+      this.destination = await response.json();
     },
+  },
+  async created() {
+    this.initData();
+    // this.$watch(() => this.$route.params, this.initData);
   },
 };
 </script>
