e562936271cb622f432556f209ba9c67e8012c57
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-08-08 07:28:15 +0200

    named routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,19 +8,16 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/destination/:id/:slug",
+    name: "destination.show",
+    component: () => import("@/views/DestinationShow.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkActiveClass: "active-link",
 });
 
 export default router;
