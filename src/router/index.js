4c6758a530d59b13ce1130e2a9d52010f71c8341
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-08-08 22:13:17 +0200

    route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
+import sourceData from "@/data.json";
 
 const routes = [
   {
@@ -12,6 +13,20 @@ const routes = [
     name: "destination.show",
     component: () => import("@/views/DestinationShow.vue"),
     props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to) {
+      const exists = sourceData.destinations.find(
+        (destination) => destination.id === parseInt(to.params.id)
+      );
+      if (!exists)
+        return {
+          name: "NotFound",
+          // allows keeping the URL while rendering a different page
+          params: { pathMatch: to.path.split("/").slice(1) },
+          query: to.query,
+          hash: to.hash,
+        };
+    },
+
     children: [
       {
         path: ":experienceSlug",
